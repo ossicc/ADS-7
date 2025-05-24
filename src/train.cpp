e0edd9318eb0f455f932b8a061aeadb4373c9a90
ossicc <ossic8881@gmail.com>
2025-05-24 22:26:26 +0300

    Update train.cpp

--- src/train.cpp
+++ src/train.cpp
@@ -3,38 +3,38 @@
 
 Train::Train() : countOp(0), first(nullptr), current(nullptr) {}
 
-void Train::addCar(bool light) {
-  Car* newCar = new Car{light, nullptr, nullptr};
-  if (!first) {
-    first = newCar;
-    first->next = first;
-    first->prev = first;
-    current = first;
-  } else {
-    Car* tail = first->prev;
-    tail->next = newCar;
-    newCar->prev = tail;
-    newCar->next = first;
-    first->prev = newCar;
-  }
-}
+ void Train::addCar(bool light) {
+   Car* newCar = new Car{light, nullptr, nullptr};
+   if (!first) {
+     first = newCar;
+     first->next = first;
+     first->prev = first;
+     current = first;
+   } else {
+     Car* tail = first->prev;
+     tail->next = newCar;
+     newCar->prev = tail;
+     newCar->next = first;
+     first->prev = newCar;
+   }
+ }
 
-int Train::getLength() {
-  if (!first) {
-    countOp = 0;
-    return 0;
-  }
-  
-  int length = 1;
-  bool lightOffExists = !first->light;
+ int Train::getLength() {
+   if (!first) {
+     countOp = 0;
+     return 0;
+   }
 
-  for (const Car* node = first->next; node != first; node = node->next) {
-    if (!node->light) lightOffExists = true;
-    ++length;
-  }
+   int length = 1;
+   bool lightOffExists = !first->light;
 
-  countOp = lightOffExists ? length * 2 : length * (length + 1);
-  return length;
-}
+   for (const Car* node = first->next; node != first; node = node->next) {
+     if (!node->light) lightOffExists = true;
+     ++length;
+   }
 
-int Train::getOpCount() const { return countOp; }
+   countOp = lightOffExists ? length * 2 : length * (length + 1);
+   return length;
+ }
+
+ int Train::getOpCount() const { return countOp; }
